d70e4775dfcb33a27f919190836c30736b76f824
Pesquisador Stefano <65348662+Pesquisador-Stefano@users.noreply.github.com>
2026-04-10 11:35:37 +0100

    Removed leftover line

--- src/BarracksServer/BarracksServer.cs
+++ src/BarracksServer/BarracksServer.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Melia.Barracks.Database;
 using Melia.Barracks.Events;
 using Melia.Barracks.Network;
